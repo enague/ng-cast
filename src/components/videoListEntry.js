f0ba055f93b1a5f92b1ddeae08f34361b1efc2a8
enague <ennague@gmail.com>
2018-05-11 17:34:52 -0700

    finished title and description for VEL

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -3,8 +3,10 @@ angular.module('video-player')
   bindings: {
     item: '<'
   },
-  controller: function ($scope) {
+  controller: function () {
     console.log('hello from VLE');
+    console.log(this);
+  
   },
   templateUrl: "src/templates/videoListEntry.html"
 });
