4d703f53da8a7d6de2ac90423e934defc5bdb91b
enague <ennague@gmail.com>
2018-05-12 10:56:40 -0700

    starting search

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,19 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function(query, callback){
+  $http({
+  method: 'GET',
+  url: '/youtube/v3/search',
+  params: {
+    q: query,
+    maxResults: 5,
+    key: window.YOUTUBE_API_KEY,
+    videoEmbeddable: 'true'
+  }
+  }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+      callback(response);
+    // when the response is available
+  }, function errorCallback(response) {
+      console.error(response)
+  });
 });
